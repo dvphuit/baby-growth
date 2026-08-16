78ddc60680c11901f661c09986dfbfc0c59df0ec
Phu Doan <61218678+dvphuit@users.noreply.github.com>
2026-08-16 15:11:14 +0700

    refactor: remove ai and score app wiring

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useUIStore } from './store/useUIStore';
 import { Header } from './components/common/Header';
 import { BottomNav } from './components/common/BottomNav';
 import { ToastContainer } from './components/common/Toast';
@@ -16,7 +15,6 @@ import { useAppModals } from './hooks/useAppModals';
 import { useReminderLifecycle } from './hooks/useReminderLifecycle';
 
 export const AppContent: React.FC = () => {
-  const { currentSubView, setCurrentSubView } = useUIStore();
   const location = useLocation();
   const isProfilePage = location.pathname === '/profile';
 
@@ -34,26 +32,17 @@ export const AppContent: React.FC = () => {
     <div className="app-container" id="appContainer">
       <ToastContainer toasts={toasts} />
 
-      {!isProfilePage && (
-        <Header
-          onOpenAiChat={() => modals.openAiChat()}
-          onOpenNotifications={modals.openNotifications}
-        />
-      )}
+      {!isProfilePage && <Header onOpenNotifications={modals.openNotifications} />}
 
       <main id="appMainContent" className="view-content-wrapper">
         <PWAInstallPrompt />
 
         <AppRoutes
-          currentSubView={currentSubView}
-          onBackFromScoreDetail={() => setCurrentSubView(null)}
-          onOpenScoreDetail={() => setCurrentSubView('score-detail')}
           onOpenQuickLog={modals.openQuickLog}
-          onOpenAiChat={() => modals.openAiChat()}
           onOpenPumping={modals.openAddPumping}
           onShowToast={addToast}
           onOpenLightbox={modals.openLightbox}
-          onOpenAddTimelineEntry={() => modals.openAddPost(undefined)}
+          onOpenAddTimelineEntry={() => modals.handleQuickAction('diary')}
           onOpenAddGrowth={modals.openAddGrowth}
           onOpenAddExpense={modals.openAddExpense}
           onOpenEditProfile={modals.openEditProfile}
@@ -63,13 +52,9 @@ export const AppContent: React.FC = () => {
       </main>
 
       <AppVersionBadge />
-
       <BottomNav onOpenQuickLog={modals.openQuickLog} />
-
       <Lightbox mediaSrc={modals.lightboxSrc} isVideo={modals.lightboxIsVideo} onClose={modals.closeLightbox} />
-
       <AppModals modals={modals} onSuccessToast={addToast} />
-
       <PWABadge />
     </div>
   );
