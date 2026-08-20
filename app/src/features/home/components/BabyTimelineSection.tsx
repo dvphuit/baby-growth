8f4f57ad234569008aac2aa09bdb841f69549b82
Phu Doan <61218678+dvphuit@users.noreply.github.com>
2026-08-20 23:27:10 +0700

    refactor: remove redundant baby timeline wrapper

--- app/src/features/home/components/BabyTimelineSection.tsx
+++
@@ -1,9 +0,0 @@
-import { IdleHomeTimelinePreview } from './IdleHomeTimelinePreview';
-
-interface BabyTimelineSectionProps {
-  owner?: 'baby' | 'mom';
-}
-
-export function BabyTimelineSection({ owner = 'baby' }: BabyTimelineSectionProps) {
-  return <IdleHomeTimelinePreview owner={owner} />;
-}
